7f5aa87e817eebd82eb6d80faccd1949177f0f3e
loganathanravi <loguaustin7788@gmail.com>
2023-04-11 13:52:08 +0530

    img

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App(){
     return(
     <div>
         <SearchBar onSubmit={handleSubmit}/>
-        <ImageList Images={images}/>
+        <ImageList images={images}/>
 
     </div>
     );
