e4e00f1570652a1377f6396bc927de1a0fa3d69d
loganathanravi <loguaustin7788@gmail.com>
2023-04-12 12:01:21 +0530

    little bit styling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,3 +1,4 @@
+import "./SearchBar.css";
 import { useState } from "react";
 function SearchBar({onSubmit}){
     const [term,SetTerm]=useState("");
@@ -9,7 +10,8 @@ function SearchBar({onSubmit}){
     SetTerm(event.target.value);
     };
 
-    return(<div>
+    return(
+    <div className="search-bar">
         <form onSubmit={handleFormSubmit}>
              <input value={term} onChange={handleChange}/>
         </form>
